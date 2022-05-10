0bae89752de603d437989784a0061900ae04b26d
Thiago Veronezi <tveronezi@gmail.com>
2022-05-10 18:52:42 -0400

    loading universe

--- src/components/bean.rs
+++ src/components/bean.rs
@@ -9,7 +9,18 @@ pub struct Props {
 
 #[function_component(Bean)]
 pub fn bean(props: &Props) -> Html {
+    let (x, y) = (
+        props.value.line * crate::universe::CELL_SIZE,
+        props.value.column * crate::universe::CELL_SIZE,
+    );
+    let style = format!(
+        "height: {}px; width: {}px; top: {}px; left: {}px",
+        crate::universe::CELL_SIZE,
+        crate::universe::CELL_SIZE,
+        x,
+        y
+    );
     html! {
-        <div>{format!("{:?}", props.value)}</div>
+        <div class="bg-blue-300 absolute" style={ style }/>
     }
 }
--- src/components/msg_ctx.rs
+++ src/components/msg_ctx.rs
@@ -1,4 +1,3 @@
-
 use std::rc::Rc;
 
 use yew::prelude::*;
