34b06d81c825713d64c1673fb30998b41c04b628
Thiago Veronezi <tveronezi@gmail.com>
2022-05-24 09:04:41 -0400

    import

--- src/components/universe_ctx.rs
+++ src/components/universe_ctx.rs
@@ -1,23 +1,20 @@
+use super::Dimensions;
+use crate::universe::{Entity, Universe, CELL_SIZE};
 use rand::Rng;
 use std::rc::Rc;
-
 use yew::prelude::*;
 
-use crate::universe;
-
-use super::Dimensions;
-
-fn random_universe(universe: &mut universe::Universe) {
+fn random_universe(universe: &mut Universe) {
     universe.entities.clear();
     let window = window_dimensions();
-    let max_y = window.height / universe::CELL_SIZE;
-    let max_x = window.width / universe::CELL_SIZE;
+    let max_y = window.height / CELL_SIZE;
+    let max_x = window.width / CELL_SIZE;
     let mut rng = rand::thread_rng();
     for y in 0..max_y {
         for x in 0..max_x {
             let number = rng.gen_range(0..10);
             if number == 7 {
-                add_entity(universe, x * universe::CELL_SIZE, y * universe::CELL_SIZE)
+                add_entity(universe, x * CELL_SIZE, y * CELL_SIZE)
             }
         }
     }
@@ -41,10 +38,10 @@ pub fn window_dimensions() -> Dimensions {
     Dimensions { height, width }
 }
 
-fn add_entity(universe: &mut universe::Universe, x: i32, y: i32) {
-    let column = x / universe::CELL_SIZE;
-    let line = y / universe::CELL_SIZE;
-    let entity = universe::Entity { line, column };
+fn add_entity(universe: &mut Universe, x: i32, y: i32) {
+    let column = x / CELL_SIZE;
+    let line = y / CELL_SIZE;
+    let entity = Entity { line, column };
     universe.entities.insert(entity);
 }
 
@@ -52,38 +49,30 @@ pub enum Command {
     Shuffle,
     Clear,
     Play,
-    Tick,
-    Iddle,
 }
 
-impl Reducible for universe::Universe {
+impl Reducible for Universe {
     type Action = Command;
 
     fn reduce(self: Rc<Self>, action: Self::Action) -> Rc<Self> {
         Rc::new(match action {
             Command::Shuffle => {
-                let mut universe = universe::Universe::new("");
+                let mut universe = Universe::new("");
                 random_universe(&mut universe);
                 universe.tick();
                 universe
             }
-            Command::Clear => universe::Universe::new(""),
+            Command::Clear => Universe::new(""),
             Command::Play => {
                 let mut universe = (*self).clone();
                 universe.tick();
                 universe
             }
-            Command::Tick => {
-                let mut universe = (*self).clone();
-                universe.tick();
-                universe
-            }
-            Command::Iddle => (*self).clone(),
         })
     }
 }
 
-pub type MessageContext = UseReducerHandle<universe::Universe>;
+pub type MessageContext = UseReducerHandle<Universe>;
 
 #[derive(Properties, Debug, PartialEq)]
 pub struct UniverseProviderProps {
@@ -94,12 +83,11 @@ pub struct UniverseProviderProps {
 #[function_component(UniverseProvider)]
 pub fn provider(props: &UniverseProviderProps) -> Html {
     let universe = use_reducer(|| {
-        let mut universe = universe::Universe::new("");
+        let mut universe = Universe::new("");
         random_universe(&mut universe);
         universe.tick();
         universe
     });
-
     html! {
         <ContextProvider<MessageContext> context={universe}>
             {props.children.clone()}
